643027294cde94fa010c4519ad877f77c6556801
Toms Esmits <smot1@inbox.lv>
2020-12-01 00:32:44 +0200

    Added SASS

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+	return gulp.src('assets/sass/**/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('assets/css'))
+});
+
+gulp.task('watch', function(){
+	gulp.watch('assets/sass/**/*.scss', gulp.series(['sass']));
+});
