ab50d4cd4b01cda0a76cb2b96dc2ed5c8ca6746c
Toms Esmits <smot1@inbox.lv>
2021-11-06 21:20:42 +0200

    Refactoring: Fetch API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,35 @@
-function buttonListener() {
-  document.querySelector('#get-movie').addEventListener('click', getMovies);
-}
+const getMovieBtn = document.querySelector('#get-movie');
 
-function getMovies(e) {
-  const title = document.querySelector('input[type="text"]').value;
-
-  const xhr = new XMLHttpRequest();
-
-  xhr.open('GET', `http://www.omdbapi.com?s=${title}&apikey=f1196e81&plot=full`, true);
-
-  xhr.onload = function() {
-    if(this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      console.log(response);
-
-      let output = '';
-
-       if(response.Response === "True") {
-        response.Search.forEach(function(movie){
-          output += `
-            <div class="col-sm-6 col-lg-4">
-              <div class="card shadow-lg">
-                <img src="${movie.Poster}">
-                <h5>${movie.Title}</h5>
-                <a onclick="movieInfo('${movie.imdbID}')" class="btn btn-lg btn-block btn-outline-light" href="#">Movie Info</a>
-              </div>
+if(getMovieBtn) {
+  getMovieBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+  
+    const title = document.querySelector('input[type="text"]').value;
+  
+    await fetch(`https://omdbapi.com/?s=${title}&apikey=f1196e81&plot=full&page=`)
+    .then(response => response.json())
+    .then(result => {
+      let movieTemplate = "";
+      if (result.Response === "True") {
+        result.Search.forEach(movie => {
+          movieTemplate += `
+          <div class="col-sm-6 col-lg-4">
+            <div class="card shadow-lg">
+              <img src="${movie.Poster}">
+              <h5>${movie.Title}</h5>
+              <button onclick="movieInfo('${movie.imdbID}')" class="btn btn-lg btn-block btn-outline-light" title="Movie Info">Movie Info</button>
             </div>
-          `;
+          </div>`;
         });
-      } else {
-        output += '<div class="col-12"><p class="text-danger text-uppercase"> Enter Correct Title</p></div>';
       }
-
-      document.querySelector('#movies').innerHTML = output;
-    }
-  }
-
-  xhr.send();
-
-  e.preventDefault();
-}
+  
+      document.querySelector('#movies').innerHTML = movieTemplate;
+    })
+    .catch(error => {
+      console.log("Error(something whent wrong)", error);
+    });
+  });
+}  
 
 function movieInfo(id) {
   sessionStorage.setItem('Movie ID', id);
@@ -47,41 +37,31 @@ function movieInfo(id) {
   return false;
 }
 
-function getSingleMovie() {
+async function getSingleMovie() {
   let singleMovieID = sessionStorage.getItem('Movie ID');
-  console.log(singleMovieID);
 
-  const xh = new XMLHttpRequest();
+  await fetch(`https://omdbapi.com/?i=${singleMovieID}&apikey=f1196e81`)
+  .then(response => response.json())
+  .then(singleResponse => {
+    let output = `
+    <div class="row">
+      <div class="col-12">
+      <h1 class="movie-heading">${singleResponse.Title}</h1>
+      </div>
+      <div class="col-md-5 d-flex align-items-center justify-content-center">
+        <img class="single-movie-img" src="${singleResponse.Poster}">
+      </div>
+      <div class="col-md-7 mt-4 mt-md-0">
+        <ul class="movie-info-list shadow-lg">
+          <li class="list-item"><span>Director: </span> ${singleResponse.Director}</li>
+          <li class="list-item"><span>Genre: </span> ${singleResponse.Genre}</li>
+          <li class="list-item"><span>IMDB Rating: </span> ${singleResponse.imdbRating}</li>
+          <li class="list-item"><span>Description: </span> ${singleResponse.Plot}</li>
+          <li class="list-item mt-4"><a class="btn btn-light" href="https://www.imdb.com/title/${singleResponse.imdbID}/" target="_blank" title="Read More!">IMDB Link</a></li>
+        </ul>
+      </div>
+    </div>`;
 
-  xh.open('GET', `http://www.omdbapi.com/?i=${singleMovieID}&apikey=f1196e81`, true);
-
-  xh.onload = function() {
-    if(this.status === 200) {
-      const singleResponse = JSON.parse(this.responseText);
-      console.log(singleResponse);
-
-      let output = `
-        <div class="row">
-          <div class="col-12">
-          <h1 class="movie-heading">${singleResponse.Title}</h1>
-          </div>
-          <div class="col-md-5 d-flex align-items-center justify-content-center">
-            <img class="single-movie-img" src="${singleResponse.Poster}">
-          </div>
-          <div class="col-md-7 mt-4 mt-md-0">
-            <ul class="movie-info-list shadow-lg">
-              <li class="list-item"><span>Director: </span> ${singleResponse.Director}</li>
-              <li class="list-item"><span>Genre: </span> ${singleResponse.Genre}</li>
-              <li class="list-item"><span>IMDB Rating: </span> ${singleResponse.imdbRating}</li>
-              <li class="list-item"><span>Description: </span> ${singleResponse.Plot}</li>
-              <li class="list-item mt-4"><a class="btn btn-light" href="https://www.imdb.com/title/${singleResponse.imdbID}/" target="_blank" title="Read More!">IMDB Link</a></li>
-            </ul>
-          </div>
-        </div>
-      `;
-
-      document.querySelector('#movie').innerHTML = output;
-    }
-  }
-  xh.send();
+  document.querySelector('#movie').innerHTML = output;
+  });
 }
